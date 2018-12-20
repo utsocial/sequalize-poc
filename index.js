02e03159ad601000860220f33c8b2a3162f06f05
Ivan Rogelio Chenoweth Chenoweth <ivan chenoweth@tiempodev.local>
2018-12-20 15:10:10 -0700

    master : updating sequelize version and displaying messages

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var models = require('./models/index.js');
 
 function startApp() {
+    console.log('Scanning the quote model:');
     models.quote.findAll().then(function(quote) {
         for( var i = 0; i < quote.length; i++ ) {
             console.log(quote[i].id + " | " + quote[i].quote);
@@ -8,6 +9,7 @@ function startApp() {
     });
 }
 
+// Sync all defined models to the DB
 models.sequelize.sync()
 .then(function() {
     startApp();
